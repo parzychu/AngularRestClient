51bd5c2ba7b1c6d5d5a699d459865caa75947664
parzychu <parzych94@gmail.com>
2015-06-01 07:58:47 +0200

    Usunieto zbedne rzeczy

--- public_html/js/directives.js
+++ public_html/js/directives.js
@@ -1,18 +1,4 @@
 var clientDirectives = angular.module('clientDirectives', ['clientControllers']);
-clientDirectives.directive("ngErrorMessage", function() {
-    return function(scope, element, attrs) {
-        element.text(scope.message + "" + attrs.message)
-    }
-})
-
-clientDirectives.directive("enter", function() {
-    return function(scope, element, attrs) {
-        element.bind("mouseover", function() {
-            console.log('jestem nad');
-            element.addClass(attrs.enter);
-        });
-    };
-});
 clientDirectives.directive('activeStar', function() {
     return function(scope, element, attrs) {
         if (scope.note.tag === true) {
@@ -29,7 +15,6 @@ clientDirectives.directive("leave", function() {
         element.bind("mouseleave", function() {
             if (scope.note.tag === false) {
                 element.removeClass(attrs.enter);
-                //       element.addClass('btn-default');
             }
             else {
                 element.addClass("active");
@@ -58,28 +43,4 @@ clientDirectives.directive("toggleColor", function() {
             });
         }
     }
-});
-clientDirectives.directive("hideMe", function($animate) {
-    return function(scope, element, attrs) {
-        scope.$watch(attrs.hideMe, function(newVal) {
-            if (newVal) {
-                $animate.addClass(element, "fade");
-            } else {
-                $animate.removeClass(element, "fade");
-            }
-        });
-    };
-});
-
-clientDirectives.animation(".toggle", function () {
-    return {
-        leave: function (element, done) {
-            element.text("Nooooo!!!");
-            TweenMax.to(element, 1, {opacity:1});
-        },
-        enter: function (element, done) {
-            element.text("Yay, I'm alive!");
-            TweenMax.from(element, 1, {opacity:0});
-        }
-    };
 });
